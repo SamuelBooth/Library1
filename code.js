040f32d66c15cd295741eff9b4663051f38f1326
SamuelBooth <sbooth4@my.wctc.edu>
2020-10-27 14:29:55 -0500

    Initial commit

---
+++ code.js
@@ -0,0 +1,39 @@
+function calculateLateFeeCost() {
+    //How many late books do you have?
+    var amountOfLateBooks = prompt("How many late books do you have?");
+
+    //convert to a number
+    var amountOfLateBooks = parseInt(amountOfLateBooks);
+
+    //How many Late DVDs do you have?
+    var amountOfLateDvds = prompt("How many late Dvd's do you have?");
+
+    //convert to a number
+    var amountOfLateDvds = parseInt(amountOfLateDvds);
+
+    //get the late fee cost of a book
+    var lateFeePerBook = 0.25;
+
+
+
+    //get the late fee cost of a dvd
+    var lateFeePerDvd = 0.50;
+
+
+
+    //find total gallons by dividing miles by MPG
+    var totalBookFeeCost = amountOfLateBooks * lateFeePerBook;
+
+    // find total gas cost by total gallons * $/gallon
+    var totalDvdFeeCost = amountOfLateDvds * lateFeePerDvd;
+
+    //find total hours by dividing miles by MPH
+    var totalLateFeeCostDisplay = totalBookFeeCost + totalDvdFeeCost;
+
+
+    // output total fee cost
+
+    alert(`Your total late fee is $${totalLateFeeCostDisplay} for late books and dvd's.`);
+
+
+}
